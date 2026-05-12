06206b35a36b53a1f2e601d944b2001cb9c0572a
Maria Fernanda <mafebonini@gmail.com>
2026-05-12 17:12:44 -0300

    SideBar

--- aplicativo_web/mvc/routes/config.js
+++ aplicativo_web/mvc/routes/config.js
@@ -1,6 +1,5 @@
 const { Router } = require("express") //usou { } para tirar uma parte do pacote
 const UsuarioController = require("../controllers/UsuarioController")
-// const AtendimentoController = require("../controllers/AtendimentoController")
 
 const router = Router()
 
@@ -11,13 +10,5 @@ router.put("/user/edit", (req,res) => UsuarioController.usuarioPutAsync(req,res)
 router.get("/user/edit/:id", (req,res) => UsuarioController.usuarioEditView(req,res))
 router.get("/user/list", (req,res) => UsuarioController.usuarioListView(req,res))
 router.delete("/user/delete/:id", (req,res) => UsuarioController.usuarioDeleteAsync(req,res))
-
-//Atendimentos
-// router.get("/listar", AtendimentoController.listar) // -> lista
-// router.get("/cadastrar", AtendimentoController.cadastrarView) // -> cadastra atendimento e salva
-// router.post("/cadastrar", AtendimentoController.cadastrar)
-// router.get("/atualizar", AtendimentoController.atualizarView) // -> atualiza atendimento e salva
-// router.post("/atualizar", AtendimentoController.atualizar)
-// router.post("/excluir", AtendimentoController.excluir) // -> excluir atendimento
 
 module.exports = router
